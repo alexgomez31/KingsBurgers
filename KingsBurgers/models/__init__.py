094dd4fc7203812f698a07b7fd763c50a8c658b0
incognito <agpan007@gmail.com>
2025-05-03 20:16:38 -0500

    migracion de categoria con productos

--- KingsBurgers/models/__init__.py
+++ KingsBurgers/models/__init__.py
@@ -2,5 +2,8 @@ from .Usuario import Usuario
 from .Cliente import Cliente
 from .Empleado import Empleado
 from .Administrador import Administrador
+from .Categoria import Categoria
+from .Producto import Producto
 
-__all__ = ['Usuario', 'Cliente', 'Empleado', 'Administrador']
+
+__all__ = ['Usuario', 'Cliente', 'Empleado', 'Administrador', 'Categoria', 'Producto']
